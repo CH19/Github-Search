2f46d8130664e2997587e0241df5bced0446bdcb
CH19 <chcc190505@gmail.com>
2023-08-30 08:36:25 -0400

    primer commit en el proyecto

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+
+const API = "https://api.github.com/users/";
+const saludo = 'Hola mundo'
+// se utiliza la variable user para obtener data de los inputs para los usuarios 
+const user = ref('');
+// funcion y metodo para cargar los usuarios de github 
+async function doSearch() {
+            const response = await fetch(API + user.value);
+            const data = await response.json();
+            console.log(data);
+            alert('solicitud llegada');
+          }
+</script>
+
+<template>
+<div class="bigContainer">
+  <div class="form-container">
+    <h3>{{saludo}}</h3>
+   <div>
+    <!-- buscador  -->
+    <input type="text" placeholder="introduce a user" v-model="user">
+    <!-- boton para buscar los usuarios en github  -->
+    <button v-on:click="doSearch">Send</button>
+    </div>
+    <hr>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+
+</style>
